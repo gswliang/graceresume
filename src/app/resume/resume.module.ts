f9c3b738805e964c93cecfbc4a3db00041c0dbe6
Grace Liang <gswliang@gmail.com>
2022-04-06 20:52:17 +0800

    combine skill & experience, managed routing

--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -6,9 +6,10 @@ import { ResumeComponent } from './resume.component';
 import { HeaderComponent } from './header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { SideNavComponent } from './sidenav/side-nav/side-nav.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
-  declarations: [ResumeComponent, HeaderComponent, SideNavComponent],
+  declarations: [ResumeComponent, HeaderComponent, SideNavComponent, SkillsComponent],
   imports: [CommonModule, ResumeRoutingModule, MatIconModule],
   exports: [HeaderComponent, SideNavComponent],
 })
--- src/app/resume/sidenav/side-nav/side-nav.component.ts
+++ src/app/resume/sidenav/side-nav/side-nav.component.ts
@@ -1,8 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 interface SideNav {
   icon: string;
   name: string;
+  data?: string;
 }
 @Component({
   selector: 'app-side-nav',
@@ -12,19 +14,30 @@ interface SideNav {
 })
 export class SideNavComponent {
   sideNav: SideNav[] = [
-    { icon: 'perm_identity', name: 'About me' },
+    { icon: 'perm_identity', name: 'About me', data: 'about' },
     {
       icon: 'build',
-      name: 'Skills',
-    },
-    {
-      icon: 'settings_accessibility',
-      name: 'Experience',
+      name: 'Skills & Experience',
+      data: 'skills',
     },
     {
       icon: 'category',
       name: 'Projects',
+      data: 'projects',
     },
   ];
   openNav = false;
+
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) {}
+
+  onClick(selectedPath: string | undefined) {
+    if (!selectedPath) {
+      return;
+    }
+
+    this.router.navigate([selectedPath], { relativeTo: this.route });
+  }
 }
