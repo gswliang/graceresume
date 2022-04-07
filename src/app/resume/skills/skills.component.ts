8b921057fe1f13b59e9be8e38b7e4055e588ad08
Grace Liang <gswliang@gmail.com>
2022-04-07 21:51:47 +0800

    Add svg icon to skills

--- src/app/resume/skills/skills.component.ts
+++ src/app/resume/skills/skills.component.ts
@@ -1,4 +1,6 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-skills',
@@ -6,9 +8,39 @@ import { Component, HostBinding, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
-  @HostBinding('class') class = 'h-screen';
+  @HostBinding('class') class = 'h-screen w-screen overflow-x-hidden';
 
-  constructor() {}
+  focusSkills = [
+    { name: 'Javascript', icon: 'javascript' },
+    { name: 'Typescript', icon: 'typescript' },
+    { name: 'Angular', icon: 'angular' },
+    { name: 'RxJs', icon: 'rxjs' },
+    { name: 'HTML 5', icon: 'html' },
+    { name: 'CSS 3', icon: 'css' },
+    { name: 'npm', icon: 'npm' },
+    { name: 'Git', icon: 'git' },
 
-  ngOnInit(): void {}
+    { name: 'NodeJs', icon: 'node' },
+  ];
+
+  usedToworkWith = [{ name: 'React', icon: 'react' }];
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit() {
+    this.focusSkills = this.focusSkills.map((skill) => {
+      return { ...skill, iconReg: this.registerSVGIcon(skill.icon) };
+    });
+  }
+
+  registerSVGIcon(icon: string) {
+    const iconUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(
+      `assets/icons/${icon}.svg`
+    );
+
+    return this.matIconRegistry.addSvgIcon(icon, iconUrl);
+  }
 }
