9ed3ecd11e533d1154b358a5e78de107cc60c6d8
Grace Liang <36319828+gswliang@users.noreply.github.com>
2022-04-14 22:46:04 +0800

    feat(Skill): implement layout and add plans (#13)
    * feat(skills): Use grid instead of flex for icon layout
    
    * feat(skill): add on plans and adjust style
    
    * ng build

--- src/app/resume/skills/skills.component.ts
+++ src/app/resume/skills/skills.component.ts
@@ -33,4 +33,38 @@ export class SkillsComponent {
       image: 'assets/icons/leetcode.png',
     },
   ];
+
+  focusOn = [
+    {
+      point: 'Currently work with Angular,Typescript,RxJS in daily basis.',
+    },
+    {
+      point:
+        'I work with CSS/SCSS at work and TailwindCSS for my own projects.',
+    },
+    {
+      point:
+        'Learning to combine Angular Material and TaiwindCSS with better approaches.',
+    },
+
+    {
+      point:
+        'Building a handy and customizable image magnifier component with npm.',
+    },
+  ];
+
+  plansDescription = [
+    {
+      plan: 'I used to practice with React, I am planning to have more project on React lately.',
+    },
+    {
+      plan: 'Building restapi and databases will makes my projects more flexible, plan to learn some backend knowledges.',
+    },
+    {
+      plan: 'Keep practicing algorithms by doing some leetcode during my free hours.',
+    },
+    {
+      plan: 'Study system design and get code clean',
+    },
+  ];
 }
